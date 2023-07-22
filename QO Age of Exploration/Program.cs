9b67819179ac33ab484620ad6e204c63212c07d6
Jared Wile <jared@siptahtv.app>
2023-07-22 17:50:11 -0400

    Initial Program: v0.0.1
    Programmer: 	Jared Wile
                    Quantum Dot Games, LLC.
                    quantumdotgames.io
    
    Description: This is a text based adventure game.

--- QO Age of Exploration/Program.cs
+++ QO Age of Exploration/Program.cs
@@ -5,21 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Program
+
 {
 	public class Program
 	{
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Quantum Odyssey: Age of Exploration!");
+            Console.WriteLine("Author: Jared Wile aka Siptah");
+            Console.WriteLine("Version: 0.0.1");
+            Console.WriteLine("Date: 07/22/2023");
 
-            Console.WriteLine("Press any key to continue...");
-            Console.Read(); // Wait for user input
+            Console.WriteLine("-------------------------------------");
 
             
+            
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("This is a text-based adventure game where you explore the universe.");
+            Console.WriteLine("You will be given a ship, and you will be able to explore the universe.");
+
+            Console.WriteLine("-------------------------------------");
+            
+            Console.WriteLine("You are a part of a crew of astronauts who have been sent to explore the galaxy.");
+            Console.WriteLine("The universe is a vast place, and you have been given the task of exploring it.");
+            Console.WriteLine("You have been given a ship, and you are an experienced pilot.");
+            Console.WriteLine("You have been dreaming of the day where you can explore the universe.");
 
 
 
-           
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("You are now in your ship, and you are ready to explore the universe.");
+            Console.WriteLine("You have been given a map of the universe, and you are ready to explore.");
+
         }
     }
 }
